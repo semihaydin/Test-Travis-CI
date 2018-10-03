1a49a5c1461cc36428f436a0ea830acde74c6a1e
Semih <hello@semihaydin.com.tr>
2018-10-03 13:20:52 +0300

    test
    Tester

--- hello.js
+++ hello.js
@@ -1,19 +1,38 @@
-var request = require("request");
+var http = require("https");
 
-var options = { method: 'POST',
-  url: 'https://www.netsparkercloud.com/api/1.0/scans/new',
-  headers: 
-   { 'Postman-Token': '42ac2bbf-d32d-4dbe-96c0-6df61c69a417',
-     'Cache-Control': 'no-cache',
-     Authorization: 'Basic ZWFjZTQ5MGY0NDliNDRkOWIzNWNhN2RhMDE4NjEwMTc6RVkzc1Z0WXVZcHgzZUR2Z2NRcisvb0FXTmtid2k5TGVJcnRuTUVnVFQxVT0=',
-     'Content-Type': 'application/json' },
-  body: 
-   { TargetUri: 'http://php.testsparker.com',
-     PolicyId: 'b20186662a01e411976c0a45dbb897e8' },
-  json: true };
+var options = {
+  "method": "POST",
+  "hostname": [
+    "www",
+    "netsparkercloud",
+    "com"
+  ],
+  "path": [
+    "api",
+    "1.0",
+    "scans",
+    "new"
+  ],
+  "headers": {
+    "Content-Type": "application/json",
+    "Authorization": "Basic ZWFjZTQ5MGY0NDliNDRkOWIzNWNhN2RhMDE4NjEwMTc6RVkzc1Z0WXVZcHgzZUR2Z2NRcisvb0FXTmtid2k5TGVJcnRuTUVnVFQxVT0=",
+    "Cache-Control": "no-cache",
+  }
+};
 
-request(options, function (error, response, body) {
-  if (error) throw new Error(error);
+var req = http.request(options, function (res) {
+  var chunks = [];
 
-  console.log(body);
+  res.on("data", function (chunk) {
+    chunks.push(chunk);
+  });
+
+  res.on("end", function () {
+    var body = Buffer.concat(chunks);
+    console.log(body.toString());
+  });
 });
+
+req.write(JSON.stringify({ TargetUri: 'http://php.testsparker.com',
+  PolicyId: 'b20186662a01e411976c0a45dbb897e8' }));
+req.end();
