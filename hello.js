cb8382d667776bf6934542449eead91126942902
Semih <hello@semihaydin.com.tr>
2018-10-03 13:41:20 +0300

    correct
    test

--- hello.js
+++ hello.js
@@ -1,38 +1,6 @@
-var http = require("https");
-
-var options = {
-  "method": "POST",
-  "hostname": [
-    "www",
-    "netsparkercloud",
-    "com"
-  ],
-  "path": [
-    "api",
-    "1.0",
-    "scans",
-    "new"
-  ],
-  "headers": {
-    "Content-Type": "application/json",
-    "Authorization": "Basic ZWFjZTQ5MGY0NDliNDRkOWIzNWNhN2RhMDE4NjEwMTc6RVkzc1Z0WXVZcHgzZUR2Z2NRcisvb0FXTmtid2k5TGVJcnRuTUVnVFQxVT0=",
-    "Cache-Control": "no-cache",
-  }
-};
-
-var req = http.request(options, function (res) {
-  var chunks = [];
-
-  res.on("data", function (chunk) {
-    chunks.push(chunk);
-  });
-
-  res.on("end", function () {
-    var body = Buffer.concat(chunks);
-    console.log(body.toString());
-  });
-});
-
-req.write(JSON.stringify({ TargetUri: 'http://php.testsparker.com',
-  PolicyId: 'b20186662a01e411976c0a45dbb897e8' }));
-req.end();
+var http = require('http');
+http.createServer(function (req, res) {
+  res.writeHead(200, {'Content-Type': 'text/plain'});
+  res.end('Hello Travis!\n'); // build should pass now!
+}).listen(1337, '127.0.0.1');
+console.log('Server running at http://127.0.0.1:1337/');
